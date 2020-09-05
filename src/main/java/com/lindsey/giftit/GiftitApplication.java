9bad80b9a4dc6579d28a50116f414aab9b5b999d
lindseyms <51429410+lindseyms@users.noreply.github.com>
2020-09-05 14:54:38 -0500

    Initial commit

---
+++ src/main/java/com/lindsey/giftit/GiftitApplication.java
@@ -0,0 +1,13 @@
+package com.lindsey.giftit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftitApplication.class, args);
+	}
+
+}
